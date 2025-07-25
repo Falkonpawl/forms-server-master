407135cb4f55d5666e0ad7b128bb90ae2366dc05
Falkonpawl <falkonpawl@gmail.com>
2025-07-25 14:43:17 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'https://front-production-89b0.up.railway.app',
+      'http://localhost:5173',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization,X-Requested-With',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('Users API')
@@ -15,14 +25,11 @@ async function bootstrap(): Promise<void> {
     .addBasicAuth()
     .addTag('users')
     .build();
-  const documentFactory = (): OpenAPIObject =>
-    SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.use(cookieParser());
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-  );
 
   app.use(
     session({
@@ -31,12 +38,23 @@ async function bootstrap(): Promise<void> {
       saveUninitialized: false,
       cookie: {
         httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 24 * 60 * 60 * 1000,
       },
     }),
   );
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 4000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
 bootstrap();
